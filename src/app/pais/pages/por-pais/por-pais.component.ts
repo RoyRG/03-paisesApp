147242fa01eee0ddbc5babcb99064901744c7648
Roy Airy Garcia Beltran <roy.garcia@qacg.com>
2022-09-28 17:15:55 -0500

    Input

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -14,8 +14,9 @@ hayError: boolean = false;
 paises: Country[] = [];
   constructor( private paisService: PaisService ) { }
 
-  buscar(){
+  buscar(termino: string ){
     this.hayError = false;
+    this.termino = termino;
     console.log(this.termino);
     this.paisService.buscarPais( this.termino )
     .subscribe({
